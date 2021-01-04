0db658d0dbfe1cdf5615df2a5388629affe267e0
Guy Dumais <dev@guydumais.digital>
2021-01-03 21:15:46 -0500

    Update 1.7.6
    CSS and Layout improvements.

--- component---src-pages-how-to-js-01177e44dfd703a8701b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{Zxbd:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),l={title:"How-To",description:"How-To stuf..."},c=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement("div",{className:"content"},o.a.createElement("h1",null,l.title),o.a.createElement("p",null,"Welcome to the How-To stuff page...")))};c.pageMeta=l,t.default=c}}]);
-//# sourceMappingURL=component---src-pages-how-to-js-01177e44dfd703a8701b.js.map
